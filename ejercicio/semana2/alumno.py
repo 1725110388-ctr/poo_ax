992d9557a4b8a6c1e583906231d00d73ec7c830b
AXEL <1725110388@utectulancingo.edu.mx>
2026-05-19 16:47:13 +0000

    Banco.py

--- ejercicio/semana2/alumno.py
+++ ejercicio/semana2/alumno.py
@@ -1,3 +1,14 @@
 class Alumno:
     def __init__(self,nombre,matriculo,edad,altura,peso,localidad,promedio,
-                 sexo,uniforme,nivelacademico):
+                 promedio,uniforme,nivel_academico):
+        self.nombre=nombre
+        self.matriculo=matriculo
+        self.edad=edad
+        self.altura=altura
+        self.peso=peso
+        self.localidad=localidad
+        self.promedio=promedio
+        self.promedio=promedio
+        self.uniforme=uniforme
+        self.nivel_academico=nivel_academico
+        print()
