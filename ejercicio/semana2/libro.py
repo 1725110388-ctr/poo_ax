8729d74dd1a61c82a7e06bed06663aa4b739e841
AXEL <1725110388@utectulancingo.edu.mx>
2026-05-21 04:09:59 +0000

    Libro

--- ejercicio/semana2/libro.py
+++ ejercicio/semana2/libro.py
@@ -21,4 +21,4 @@ class Libro:
         print(f"editorial {self.editorial}")
         print(f"año_publicado {self.año_publicado}")
         print(f"idioma {self.idioma}")
-caperusita=Libro("Caperucita Roja", "Charles Perrault", true, true, false, 30, "cuentos infatiles", "Editorial Océano", 2000, "español")
+caperusita=Libro("Caperucita Roja", "Charles Perrault", True, True, False, 30, "cuentos infatiles", "Editorial Océano", 2000, "español")
